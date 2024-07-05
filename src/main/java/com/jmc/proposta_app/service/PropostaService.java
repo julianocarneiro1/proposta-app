8cbeb1ecc0596841542596d0b7e55be77132c0f4
Juliano Carneiro <julianoomc@gmail.com>
2024-07-05 19:10:05 -0300

    referencia exchange no application.properties

--- src/main/java/com/jmc/proposta_app/service/PropostaService.java
+++ src/main/java/com/jmc/proposta_app/service/PropostaService.java
@@ -4,8 +4,8 @@ import com.jmc.proposta_app.dto.PropostaResponseDto;
 
 import com.jmc.proposta_app.entity.Proposta;
 import com.jmc.proposta_app.mapper.PropostaMapper;
-import lombok.AllArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.jmc.proposta_app.repository.PropostaRepository;
@@ -13,19 +13,29 @@ import com.jmc.proposta_app.dto.PropostaRequestDto;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Service
 public class PropostaService {
 
 	private PropostaRepository propostaRepository;
+
 	private NotificacaoService notificacaoService;
-	
+
+	private String exchange;
+
+	public PropostaService(PropostaRepository propostaRepository,
+						   NotificacaoService notificacaoService,
+						   @Value("${rabbitmq.propostapendente.exchange}") String exchange) {
+		this.propostaRepository = propostaRepository;
+		this.notificacaoService = notificacaoService;
+		this.exchange = exchange;
+	}
+
 	public PropostaResponseDto criar(PropostaRequestDto requestDto) {
 		Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDto);
 		propostaRepository.save(proposta);
 
 		PropostaResponseDto response = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
-		notificacaoService.notificar(response, "proposta-pendente.ex");
+		notificacaoService.notificar(response, exchange);
 
 		return response;
 	}
